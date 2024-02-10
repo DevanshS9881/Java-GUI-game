d4ae4641a73c73930b5e463483149111dfe53e26
DevanshS9881 <srivdevansh2112@gmail.com>
2024-02-10 14:55:56 +0530

    today commit

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main
     public static void main(String[] args)
     {
      System.out.println("Program started");
-     TGame ob=new TGame();
+     int s[]={0,0};
+     TGame ob=new TGame(s);
     } 
 }
--- TGame.java
+++ TGame.java
@@ -1,3 +1,4 @@
+
 //import java.awt.BorderLayout;
 //import java.awt.Font;
 //import java.awt.GridLayout;
@@ -12,128 +13,148 @@ import java.awt.event.*;
 //import javax.swing.JPanel;
 import javax.swing.*;
 
-
-public class TGame extends JFrame implements ActionListener
-{
+public class TGame extends JFrame implements ActionListener {
     JLabel heading;
-    Font font=new Font("",Font.BOLD,40);
+    JLabel heading2;
+    Font font = new Font("", Font.BOLD, 40);
     JPanel mainPanel;
-    //JButton []btn=new JButton[9]; 
-    int gamechances[]={2,2,2,2,2,2,2,2,2};
-    int activeP=0;
-    int count=0;
-    TGame()
+     JButton []btn=new JButton[9];
+    int gamechances[] = { 2, 2, 2, 2, 2, 2, 2, 2, 2 };
+    int activeP = 0;
+    int count = 0;
+    int score[];
+    int Windex[][]={{0,1,2},{0,3,6},{0,4,8},{8,2,5},{8,6,7},{1,4,7},{3,4,5},{2,4,6}};
+    TGame(int s[]) 
     {
         setTitle("My Tic Tac game");
-        setSize(850,850);
+        setSize(850, 850);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        score=s;
         GUIcreate();
         setVisible(true);
-        
-                
-        
-      
+
     }
-    public void GUIcreate()
-     {
-         this.getContentPane().setBackground(Color.yellow);
-         this.setLayout(new BorderLayout());
-         heading=new JLabel("TIC TAC TOE");
-         heading.setFont(font);
-         heading.setForeground(Color.green);
-         this.add(heading,BorderLayout.NORTH);
-         heading.setHorizontalAlignment(SwingConstants.CENTER);
-         //panel section
-          mainPanel=new JPanel();
-          mainPanel.setLayout(new GridLayout(3,3));
-          for(int i=1;i<=9;i++)
-          {
-              JButton  bt=new JButton();
-              bt.setBackground(Color.cyan);
-              bt.setFont(font);
-              mainPanel.add(bt);
-              //btn[i-1]=bt;
-              bt.addActionListener(this);
-              bt.setName(String.valueOf(i-1));
-          } 
-          this.add(mainPanel,BorderLayout.CENTER);
-         
-     }  
+
+    public void GUIcreate() {
+        this.getContentPane().setBackground(Color.yellow);
+        this.setLayout(new BorderLayout());
+        heading = new JLabel("TIC TAC TOE");
+        heading2=new JLabel("SCOREBOARD    PLAYER 0: "+score[0]+"     PLAYER 1: "+score[1]);
+        heading.setFont(font);
+        heading.setForeground(Color.green);
+        this.add(heading, BorderLayout.NORTH);
+        heading.setHorizontalAlignment(SwingConstants.CENTER);
+        heading2.setFont(font);
+        heading2.setForeground(Color.black);
+        this.add(heading2, BorderLayout.SOUTH);
+        heading2.setHorizontalAlignment(SwingConstants.CENTER);
+        // panel section
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(3, 3));
+        for (int i = 1; i <= 9; i++) {
+            JButton bt = new JButton();
+            bt.setBackground(Color.cyan);
+            bt.setFont(font);
+            mainPanel.add(bt);
+             btn[i-1]=bt;
+            bt.addActionListener(this);
+            bt.setName(String.valueOf(i - 1));
+        }
+        this.add(mainPanel, BorderLayout.CENTER);
+
+    }
 
     @Override
-    public void actionPerformed(ActionEvent e) 
-    {
+    public void actionPerformed(ActionEvent e) {
         System.out.println("clicked");
-        JButton currentB =(JButton)e.getSource();
-        String name=currentB.getName();
-        int n=Integer.parseInt(name.trim());
-        if(gamechances[n]==2)
+        JButton currentB = (JButton) e.getSource();
+        String name = currentB.getName();
+        int n = Integer.parseInt(name.trim());
+        int i=0;
+        int s[];
+        if (gamechances[n] == 2) 
         {
-            if(activeP==1)
+            if (activeP == 1) 
             {
                 currentB.setText("X");
-                activeP=0;
-                gamechances[n]=1;
+                activeP = 0;
+                gamechances[n] = 1;
                 count++;
-                if(Logic()==true)
-                {
-                    System.exit(0);
-                }
-            }
-            else
+                
+            } else 
             {
                 currentB.setText("O ");
-                activeP=1;
-                gamechances[n]=0;
+                activeP = 1;
+                gamechances[n] = 0;
                 count++;
-                if(Logic()==true)
+            }
+            //winner logic
+            for(int[] temp: Windex)
+            {
+                if(gamechances[temp[0]]==gamechances[temp[1]]&&(gamechances[temp[0]]==gamechances[temp[2]])&&(gamechances[temp[0]]!=2))
                 {
-                    System.exit(0);
+                    btn[temp[0]].setBackground(Color.red);
+                    btn[temp[1]].setBackground(Color.red);
+                    btn[temp[2]].setBackground(Color.red);
+                    score[gamechances[temp[0]]]++;
+                    JOptionPane.showMessageDialog(this,"Player "+gamechances[temp[0]]+" Wins!!");
+                    //JoptionPane.showMessageDialog(this,"SCORES")
+                    //System.exit(0);
+                    i=JOptionPane.showConfirmDialog(this, "Do you want to play more");
+                    if(i==0)
+                    {
+                        this.setVisible(false);
+                        new TGame(score);
+                       
+                    }
+                    else 
+                    {
+                      System.exit(0);
+
+                    }
                 }
             }
-        }
-        else
-        {
-            JOptionPane.showMessageDialog(this,"Position already occupied!!!!");
-        }
+             if(count==9)
+                {
+                        JOptionPane.showMessageDialog(this, "MATCH DRAW!!!!");
+                        System.exit(0);
+                }
                 
-        
-        
-    }
-    public boolean Logic()
-    {
-    if(((gamechances[0]==gamechances[1])&&(gamechances[2]==gamechances[1])&&(gamechances[1]!=2))||
-            ((gamechances[0]==gamechances[3])&&(gamechances[6]==gamechances[0])&&(gamechances[0]!=2))||
-            ((gamechances[0]==gamechances[4])&&(gamechances[8]==gamechances[0])&&(gamechances[0]!=2))||
-            ((gamechances[8]==gamechances[2])&&(gamechances[5]==gamechances[8])&&(gamechances[8]!=2))||
-            ((gamechances[8]==gamechances[0])&&(gamechances[4]==gamechances[8])&&(gamechances[8]!=2))||
-            ((gamechances[8]==gamechances[6])&&(gamechances[7]==gamechances[8])&&(gamechances[8]!=2))||
-            ((gamechances[1]==gamechances[4])&&(gamechances[7]==gamechances[1])&&(gamechances[1]!=2))||
-            ((gamechances[3]==gamechances[4])&&(gamechances[5]==gamechances[4])&&(gamechances[4]!=2)))
-       
 
-    {
-        if(activeP==1)
-        {
-            JOptionPane.showMessageDialog(this,"Player O Wins!!!!");
-            return true;
-        }
-        else
-        {
-            JOptionPane.showMessageDialog(this,"Player X Wins !!!!");
-            return true;
-        }
-    }
-    else 
-    {
-        if(count==9)
-            {
-            JOptionPane.showMessageDialog(this,"MATCH DRAW!!!!");
-            System.exit(0);
             
-            }
-        return false;
+        } 
+        else {
+            JOptionPane.showMessageDialog(this, "Position already occupied!!!!");
+        }
+
     }
-    }
+
+    //public boolean Logic() {
+        //if (((gamechances[0] == gamechances[1]) && (gamechances[2] == gamechances[1]) && (gamechances[1] != 2)) ||
+                //((gamechances[0] == gamechances[3]) && (gamechances[6] == gamechances[0]) && (gamechances[0] != 2)) ||
+                //((gamechances[0] == gamechances[4]) && (gamechances[8] == gamechances[0]) && (gamechances[0] != 2)) ||
+                //((gamechances[8] == gamechances[2]) && (gamechances[5] == gamechances[8]) && (gamechances[8] != 2)) ||
+                //((gamechances[8] == gamechances[0]) && (gamechances[4] == gamechances[8]) && (gamechances[8] != 2)) ||
+                //((gamechances[8] == gamechances[6]) && (gamechances[7] == gamechances[8]) && (gamechances[8] != 2)) ||
+                //((gamechances[1] == gamechances[4]) && (gamechances[7] == gamechances[1]) && (gamechances[1] != 2)) ||
+                //((gamechances[3] == gamechances[4]) && (gamechances[5] == gamechances[4]) && (gamechances[4] != 2)))
+
+        //{
+            //if (activeP == 1) {
+                //JOptionPane.showMessageDialog(this, "Player O Wins!!!!");
+                //return true;
+           // } else {
+                //JOptionPane.showMessageDialog(this, "Player X Wins !!!!");
+               // return true;
+           // }
+       // } else {
+           // if (count == 9) {
+                //JOptionPane.showMessageDialog(this, "MATCH DRAW!!!!");
+                //System.exit(0);
+
+           // }
+            //return false;
+       // }
+    //}
+
 }
- 
